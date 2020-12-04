f298a6158b2e84c4b78746be9214d36966f0f5bc
lenovo <shaplykon@gmail.com>
2020-12-04 19:32:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/battleship/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.battleship;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 1;
+    Button signInButton;
+    Button profileButton;
+    TextView usernameLabel;
+    FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        usernameLabel = findViewById(R.id.usernameLabel);
+        mAuth = FirebaseAuth.getInstance();
+
+        profileButton =  findViewById(R.id.profileButton);
+        profileButton.setOnClickListener(v-> {
+            if(mAuth.getCurrentUser() != null){
+                Intent profileIntent = new Intent(getApplicationContext(), ProfileActivity.class);
+                startActivity(profileIntent);
+            }
+        });
+
+        signInButton = findViewById(R.id.signInButton);
+        signInButton.setOnClickListener(v -> {
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.EmailBuilder().build(),
+                    new AuthUI.IdpConfig.GoogleBuilder().build());
+
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                    RC_SIGN_IN);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                FirebaseUser user = mAuth.getCurrentUser();
+                updateUI();
+            } else {
+                Toast.makeText(this, "Problem ocurred while authenticating", Toast.LENGTH_SHORT).show();
+                        
+            }
+        }
+    }
+    
+    @SuppressLint("SetTextI18n")
+    private void updateUI(){
+        usernameLabel.setText("You are welcome, "  + Objects.requireNonNull(mAuth.getCurrentUser()).getDisplayName());
+        
+    }
+}
