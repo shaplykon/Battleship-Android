f61d49c72f1e0f5278b806fbdf002520196de8b9
lenovo <shaplykon@gmail.com>
2020-12-08 18:43:08 +0300

    Added Gravatar Profile image support.

---
+++ app/src/main/java/com/example/battleship/MD5Util.java
@@ -0,0 +1,25 @@
+package com.example.battleship;
+
+import java.util.*;
+import java.io.*;
+import java.security.*;
+
+public class MD5Util {
+    public static String hex(byte[] array) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : array) {
+            sb.append(Integer.toHexString((b
+                    & 0xFF) | 0x100).substring(1, 3));
+        }
+        return sb.toString();
+    }
+    public static String md5Hex (String message) {
+        try {
+            MessageDigest md =
+                    MessageDigest.getInstance("MD5");
+            return hex (md.digest(message.getBytes("CP1252")));
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException ignored) {
+        }
+        return null;
+    }
+}
--- app/src/main/java/com/example/battleship/ProfileActivity.java
+++ app/src/main/java/com/example/battleship/ProfileActivity.java
@@ -1,25 +1,21 @@
 package com.example.battleship;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
+import android.preference.PreferenceManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.material.switchmaterial.SwitchMaterial;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Objects;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -30,6 +26,11 @@ public class ProfileActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     FirebaseUser currentUser;
+
+    SwitchMaterial gravatarSwitch;
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,22 +47,48 @@ public class ProfileActivity extends AppCompatActivity {
         usernameEdit.setText(currentUser.getDisplayName());
         emailEdit.setText(currentUser.getEmail());
         Uri profileImageUri;
-        if(currentUser.getPhotoUrl() != null){
-            profileImageUri = Uri.parse(currentUser.getPhotoUrl().toString());
+
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = sharedPreferences.edit();
+
+
+        boolean gravatarUsing = sharedPreferences.getBoolean("gravatarUsing", false);
+
+        gravatarSwitch = findViewById(R.id.gravatarSwitch);
+
+        gravatarSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            editor.putBoolean("gravatarUsing", isChecked);
+            editor.apply();
+            // TODO Need to add activity restarting
+            //finish();
+            //tartActivity(new Intent(ProfileActivity.this, ProfileActivity.this.getClass()));
+        });
+
+        gravatarSwitch.setChecked(gravatarUsing);
+        if (gravatarUsing) {
+            String email = currentUser.getEmail();
+            assert email != null;
+            String hash = MD5Util.md5Hex(email.trim().toLowerCase());
+            profileImageUri = Uri.parse("https://www.gravatar.com/avatar/" + hash + "?size=256");
+        } else {
+            if (currentUser.getPhotoUrl() != null) {
+                profileImageUri = Uri.parse(currentUser.getPhotoUrl().toString());
+            } else {
+                profileImageUri = Uri.parse("https://yt3.ggpht.com/a/AATXAJwqEcDwWfOxAKdtQUUSm-" +
+                        "lLCopCNoBdoFlTv9BsTA=s900-c-k-c0xffffffff-no-rj-mo");
+            }
         }
-        else{
-            profileImageUri = Uri.parse("https://yt3.ggpht.com/a/AATXAJwqEcDwWfOxAKdtQUUSm-lLCopCNoBdoFlTv9BsTA=s900-c-k-c0xffffffff-no-rj-mo");
-        }
-        SimpleDraweeView draweeView =  findViewById(R.id.profileImage);
-        draweeView.setImageURI(profileImageUri);
+        SimpleDraweeView profileImageView = findViewById(R.id.profileImage);
+        profileImageView.setImageURI(profileImageUri);
 
         signOutButton = findViewById(R.id.signOutButton);
         signOutButton.setOnClickListener(v -> AuthUI.getInstance().signOut(getApplicationContext()).addOnCompleteListener(task -> {
             Toast.makeText(ProfileActivity.this, "Successfully signed out!", Toast.LENGTH_SHORT).show();
             Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(mainIntent);
-        }));
 
+        }));
 
     }
 }
